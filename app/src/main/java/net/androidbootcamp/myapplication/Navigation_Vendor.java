3ab326960a51bbaf6f8b4e2d5269bb03fc30b57b
pcireus <pcireus@yahoo.com>
2020-03-03 01:09:55 -0500

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/myapplication/Navigation_Vendor.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Navigation_Vendor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_vendor);
+    }
+}
---
+++ app/src/main/java/net/androidbootcamp/myapplication/Vendor_Items.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Vendor_Items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_items);
+    }
+}
